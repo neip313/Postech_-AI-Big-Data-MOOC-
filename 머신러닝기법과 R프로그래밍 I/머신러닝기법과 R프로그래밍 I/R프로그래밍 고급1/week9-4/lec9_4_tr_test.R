d99bb938c3e0c2513a72246a983f641c5467e41a
neip313 <neip313@naver.com>
2020-11-25 16:33:20 +0900

    commit

--- 머신러닝기법과 R프로그래밍 I/머신러닝기법과 R프로그래밍 I/R프로그래밍 고급1/week9-4/lec9_4_tr_test.R
+++ 머신러닝기법과 R프로그래밍 I/머신러닝기법과 R프로그래밍 I/R프로그래밍 고급1/week9-4/lec9_4_tr_test.R
@@ -3,7 +3,6 @@
 # training data and test data
 
 # set working directory
-setwd("D:/tempstore/moocr/wk9")
 
 # read csv file
 iris<-read.csv(file="iris.csv")
@@ -12,19 +11,18 @@ str(iris)
 attach(iris)
 
 # training/ test data : n=150
-set.seed(1000, sample.kind="Rounding")
+set.seed(100) # seed 넘버는 내 맘대로 줘도 된다. 
 N=nrow(iris)
-tr.idx=sample(1:N, size=N*2/3, replace=FALSE)
+tr.idx=sample(1:N, size=N*2/3, replace=FALSE) # 150개 데이터 중에서 2/3를 랜덤하게 샘플링한 데이터 추출 -> 3fold 가 됨 
 tr.idx
 
 # attributes in training and test
-iris.train<-iris[tr.idx,-5]
-iris.test<-iris[-tr.idx,-5]
+iris.train<-iris[tr.idx,-5] # 5번째 열의 종속변수를 제외한 100개의 데이터
+iris.test<-iris[-tr.idx,-5] # 5번째 열의 종속변수를 제외한 50개의 데이터 
 
 # target value in training and test
-trainLabels<-iris[tr.idx,5]
-testLabels<-iris[-tr.idx,5]
+trainLabels<-iris[tr.idx,5] # train의 ?타켓변수의 값이 무엇이냐 -> tr.idx의 5번째 칼럼 
+testLabels<-iris[-tr.idx,5] # test의 타켓변수 값이 무엇이냐
 
 # to get frequency of class in test set
-table(testLabels)
-
+table(testLabels)  
